e5ee4d3ad0692ac47b4745b460115fa8d2772008
DougsGit <ddziuba@dziubaassociates.com>
2023-12-19 19:23:50 -0600

    making additional upadates

--- Go/GoWebDevelopment-Udemy/main.go
+++ Go/GoWebDevelopment-Udemy/main.go
@@ -2,9 +2,53 @@ package main
 
 import "fmt"
 
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name  string
+	Breed string
+}
+
+type Gorilla struct {
+	Name          string
+	Color         string
+	NumberOfTeeth int
+}
+
 func main() {
-	firstLine :="Once upon a midnight dreary"
-	for i, letter:=range firstLine {
-		fmt.Printf("the %d letter is %v\n",i,string(letter))
+	dog := Dog{
+		Name:  "Sampson",
+		Breed: "Poodle",
 	}
+	PrintInfo(&dog)
+
+	gorilla := Gorilla{
+		Name:  "Jock",
+		Color: "grey",
+		NumberOfTeeth: 38,
+	}
+	PrintInfo(&gorilla)
+}
+
+func PrintInfo(a Animal) {
+	fmt.Printf("This animal says %v and has %d legs\n", a.Says(), a.NumberOfLegs())
+}
+
+func (d *Dog) Says() string {
+	return "Woof"
+}
+
+func (d *Dog) NumberOfLegs() int {
+	return 4
+}
+
+func (d *Gorilla) Says() string {
+	return "Ugh"
+}
+
+func (d *Gorilla) NumberOfLegs() int {
+	return 2
 }
