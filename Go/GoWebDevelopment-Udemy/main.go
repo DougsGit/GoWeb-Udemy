7995efcf0a7edc525c0318a95bf4ce14d815f41e
DougsGit <ddziuba@dziubaassociates.com>
2023-12-29 16:55:12 -0600

    resting the base web app

--- Go/GoWebDevelopment-Udemy/main.go
+++ Go/GoWebDevelopment-Udemy/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("This works")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "Hello, world!")
+		if err != nil {
+			fmt.Println(err)
+
+		}
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
 }
