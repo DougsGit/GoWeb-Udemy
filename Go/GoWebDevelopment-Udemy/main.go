60d173c5f6c4a132c46765d1d108e60afd3c7c63
DougsGit <ddziuba@dziubaassociates.com>
2023-12-30 11:18:16 -0600

    add the divide func

--- Go/GoWebDevelopment-Udemy/main.go
+++ Go/GoWebDevelopment-Udemy/main.go
@@ -1,19 +1,48 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the Home Page")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := AddValues(2, 2)
+	fmt.Fprintf(w, "This is the About Page\nSum = %d", sum)
+}
+
+func Divide(w http.ResponseWriter, r *http.Request) {
+	var x, y float32
+	x, y = 100, 0
+	f, err := DivideValues(x, y)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is equal to %f", x, y, f))
+}
+
+func DivideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("Cannot divide by 0!")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
+func AddValues(x, y int) int {
+	return x + y
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, world!")
-		if err != nil {
-			fmt.Println(err)
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
-
-	_ = http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe(":8686", nil)
 }
